fdfbcade8a52362f9dcef18b8d90503a645913cd
Sama34 <Sama34@users.noreply.github.com>
2024-12-28 11:06:13 -0800

    META 3.1.1

--- Upload/inc/plugins/newpoints/plugins/ActivityRewards/admin.php
+++ Upload/inc/plugins/newpoints/plugins/ActivityRewards/admin.php
@@ -148,8 +148,8 @@ function plugin_information(): array
         'website' => 'https://ougc.network',
         'author' => 'Omar G.',
         'authorsite' => 'https://ougc.network',
-        'version' => '3.1.0',
-        'versioncode' => 3100,
+        'version' => '3.1.1',
+        'versioncode' => 3101,
         'compatibility' => '31*',
         'codename' => 'ougc_points_activity_rewards'
         //'codename' => 'newpoints_activity_rewards'
